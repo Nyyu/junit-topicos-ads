4f90f30ec6c22697bc0fb840bed60323c7bc3a62
Nyyu <89911360+Nyyu@users.noreply.github.com>
2022-06-22 19:53:18 -0300

    first part

---
+++ src/main/java/com/example/project/Turma.java
@@ -0,0 +1,39 @@
+package com.example.project;
+
+public class Turma {
+    LinkedList<Double> bigL = new LinkedList<>();    
+    
+    public void addNota(double nota) {
+        bigL.add(nota);
+    }
+
+    public int getQuantidadeNotas() {
+        int temp = 0;
+        for(Double nota : this.bigL) {
+            temp++;
+        }
+        return temp;
+    }
+
+    public double getSoma() {
+        double temp = 0;
+        for(Double nota : this.bigL) {
+            temp += nota;
+        }
+        return temp;
+    }
+
+    public double getMediaDasNotas() {
+        return getSoma() / getQuantidadeNotas();
+    }
+
+    public int getQuantidadeNotasMeioresQueAMedia() {
+        int temp = 0;
+        for(Double nota : this.bigL) {
+            if(nota > getMediaDasNotas()) {
+                temp++;
+            }
+        }
+        return temp;
+    }
+}
